4d48b46bece16afa970668b31521c0febc8dc9d1
DHaralson89 <62124080+DHaralson89@users.noreply.github.com>
2020-06-11 20:05:37 -0500

    classwork

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,9 @@
+describe('Form - testing our form inputs', function() {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/");
+    })
+
+    it("add text to inputs and submit form", function(){
+        
+    });
+});
